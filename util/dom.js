8172b4cab2117504a72f996350e1ce35852570a0
Haya <Haya.A.S.Alqassem@gmail.com>
2024-11-20 20:22:30 +0300

    Update dom.js

--- util/dom.js
+++ util/dom.js
@@ -1,9 +1,7 @@
 export function showError(message) {
-  const errorContainerElement = document.getElementById('errors'); // Reaching out to the DOM to select an element.
-  const errorMessageElement = document.createElement('p'); // Adding a paragraph ('p')
+  const errorContainerElement = document.getElementById('errors'); 
+  const errorMessageElement = document.createElement('p'); 
   errorMessageElement.textContent = message;
-  errorContainerElement.innerHTML = ''; // Clearing the selected element "errorContainerElement".
-  /* Adding the newly created element "errorMessageElement" as a child to 
-  the cleared element "errorContainerElement" */
+  errorContainerElement.innerHTML = ''; 
   errorContainerElement.append(errorMessageElement);
 }
